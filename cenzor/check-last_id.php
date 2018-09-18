6c484bec68f7b0f2790eaaecf88b718e55bc0f71
asay1515 <36467449+asay1515@users.noreply.github.com>
2018-09-19 00:21:10 +0300

    Delete check-last_id.php

--- cenzor/check-last_id.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = mysqli_connect('localhost', 'mihopol', 'mihpol', 'Censor-bot') or die(mysqli_error($db));
-
-$query = mysqli_query($db, "SELECT LAST_INSERT_ID() FROM comments") or die(mysqli_error($db));
-$last_id = mysqli_fetch_row($query);
-
-echo $last_id[0];
-?>
