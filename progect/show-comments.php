8088f99779e6ebac25e14d4b1fddb04c698be6e2
asay1515 <36467449+asay1515@users.noreply.github.com>
2018-09-19 00:19:38 +0300

    Update show-comments.php

--- progect/show-comments.php
+++ progect/show-comments.php
@@ -1,11 +1,6 @@
 
 <?php
-$db = mysqli_connect('localhost', 'mihopol', 'mihpol', 'Censor-bot') or die(mysqli_error($db));
-$query = mysqli_query($db, "SELECT * FROM comments") or die(mysqli_error($db));
-$comments = array();
-while ($comment = mysqli_fetch_row($query)){
-    $comments[] = $comment;
-}
+
 foreach (array_reverse($comments) as $comment): ?>
     <div class="comment" id="<?=$comment[0]?>">
     <div class="name"><?=$comment[1]?></div><hr>
