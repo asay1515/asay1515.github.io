a83076301bb539f6285c8264951fb24a82ba09c1
asay1515 <36467449+asay1515@users.noreply.github.com>
2018-10-25 23:26:07 +0300

    Delete checkmat.php

--- progect/checkmat.php
+++
@@ -1,7 +0,0 @@
-<?php
-$json='{1:"х**",2:"охуевший", 3:"охреневший"}';
-$arr=json_decode($json,true);//преобразуем данные из json в массив
-for ($i=0;i<2;i++)
-if (<?php echo htmlspecialchars($_POST['name-input'])==$arr[i])//прогнать имя на наличие бана
-//вывести ошибку
-<?php>
